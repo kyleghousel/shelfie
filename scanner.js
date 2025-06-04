c0a63b6c306f797f92aad6cf57cc4ce039a5c35f
Kyle Housel <kylegranthousel@gmail.com>
2025-06-04 10:48:25 -0400

    Search OpenLibrary with ISBN to fill form

--- scanner.js
+++ scanner.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const resultEl = document.getElementById('isbn-result')
   const isbnInput = document.getElementById('book-isbn-input')
   const scanBtn = document.getElementById('start-scan-btn')
+  const titleAdd = document.querySelector('#book-title-input')
+  const authorAdd = document.querySelector('#book-author-input')
+  const coverAdd = document.querySelector('#book-cover-input')
 
   scanBtn.addEventListener('click', () => {
     Quagga.init({
@@ -29,6 +32,13 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log('Detected ISBN:', isbn)
       resultEl.textContent = `ISBN: ${isbn}`
       isbnInput.value = isbn
+      fetch(`https://openlibrary.org/search.json?q=${isbn}`)
+        .then(response => response.json())
+        .then(searchData => {
+          titleAdd.value = searchData.docs[0].title
+          authorAdd.value = searchData.docs[0].author_name[0]
+          coverAdd.value = `https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg`
+        })
       Quagga.stop()
     })
   })
